3fe8aae69be9119afb099d9a30feec4f7aecc673
Thomas-Guo <72498365+Thomas-guo007@users.noreply.github.com>
2026-06-15 18:50:27 +0800

    Register passwordless auth routes

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -4,6 +4,7 @@ import { createServer } from "http";
 import net from "net";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { registerOAuthRoutes } from "./oauth";
+import { registerPasswordlessRoutes } from "./passwordless";
 import { registerStorageProxy } from "./storageProxy";
 import { appRouter } from "../routers";
 import { createContext } from "./context";
@@ -56,6 +57,7 @@ async function startServer() {
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
   registerStorageProxy(app);
+  registerPasswordlessRoutes(app);
   registerOAuthRoutes(app);
 
   app.get("/api/health", (_req, res) => {
